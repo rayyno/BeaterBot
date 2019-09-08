b2fa2675d815dbd404ba296b8d24db24bfe3471f
rayyno <55065251+rayyno@users.noreply.github.com>
2019-09-09 02:44:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,17 @@ client.on('ready', () => {
 
 });
 
-
+client.on("ready", () => {
+  const channel = client.channels.get("mychannelid");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
+});
 
 client.on('message', message => {
 
