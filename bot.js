84317bbcd509d7d45e3242815b0042b2aac0f0f7
rayyno <55065251+rayyno@users.noreply.github.com>
2019-09-09 21:46:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,10 @@
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 
 const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
 client.on('ready', () => {
@@ -31,47 +28,56 @@ client.on("ready", () => {
   });
 });
 
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+	let fontSize = 70;
 
-client.on('message', message => {
+	do {
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+	} while (ctx.measureText(text).width > canvas.width - 300);
 
-    if (message.content === 'كسمك') {
+	return ctx.font;
+};
 
-       message.reply('كسمك انت');
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	if (!channel) return;
 
-       }
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
 
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
-client.on('message', message => {
-
-    if (message.content === 'مين سالب السيرفر؟') {
-
-       message.reply('مجودي');
-
-       }
-
+client.on('message', async message => {
+	if (message.content === '!join') {
+		client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+	}
 });
-
-client.on('message', message => {
-
-    if (message.content === 'هل الزنا حلال؟') {
-
-       message.reply('والله على حسب');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'سب الام') {
-
-       message.reply('زي السلامعليكم عندكم');
-
-       }
-
-});
-
  
 
 // THIS  MUST  BE  THIS  WAY
