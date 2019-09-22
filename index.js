2570eb0e1176bc6320725d43dcabe081a331966e
rayyno <55065251+rayyno@users.noreply.github.com>
2019-09-22 21:59:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ client.on('message', async message => {
   }
 });
 
+// For Invited by whom
 client.on("ready", () => {
     var guild;
     while (!guild)
@@ -145,32 +146,31 @@ client.on("ready", () => {
         });
     });
 });
- 
- 
-// Invited by whom  
+
 client.on("guildMemberAdd", (member) => {
-	let channel = member.guild.channels.get("اي دي الروم - Room id");
-	if (!channel) {
-		console.log("!the channel id it's not correct");
-		return;
-	}
-	if (member.id == client.user.id) {
-		return;
-	}
-	console.log('-');
-	var guild;
-	while (!guild)
-		guild = client.guilds.get("اي دي سيرفرك - Server id");
-	guild.fetchInvites().then((data) => {
-		data.forEach((Invite, key, map) => {
-			var Inv = Invite.code;
-			if (dat[Inv])
-				if (dat[Inv] < Invite.uses) {
-					channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
-				}
-			dat[Inv] = Invite.uses;
-		});
-	});
+    let channel = member.guild.channels.get("اي دي الروم - Room id");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
 });
 
 // For Sending Invite link to users
