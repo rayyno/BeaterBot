d57f9068c0d725f51b07e42aa581e0563769329d
rayyno <55065251+rayyno@users.noreply.github.com>
2019-09-22 08:03:31 +0300

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,3 +3,17 @@ client.on('message', message => {
        message.reply('كسمك انت');
        }
 });
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Bot Admin"
+};
+
+exports.help = {
+  name: "كسمك",
+  category: "كس ام امك",
+  description: "كس ام ام امك",
+  usage: "كسمك"
+};
