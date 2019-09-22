49474135fe10f05ec3ee1ad7160bb11b3d10af6e
rayyno <55065251+rayyno@users.noreply.github.com>
2019-09-22 05:46:21 +0300

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,18 @@
-exports.run = async (client, message, args) => {
-  const m = await message.channel.send("Calculating...");
-  m.edit(`Pong! :ping_pong:\nLatency é **${m.createdTimestamp - message.createdTimestamp}**ms.\nAPI Latency é **${Math.round(client.ping)}**ms.`);
-}
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const msg = await message.channel.send("Ping?");
+  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "ping",
+  category: "Miscelaneous",
+  description: "It like... Pings. Then Pongs. And it's not Ping Pong.",
+  usage: "ping"
+};
