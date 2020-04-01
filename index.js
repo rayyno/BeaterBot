a991b8bf8ed91ed56cf4e1ab9c511dc9744c6341
rayyno <55065251+rayyno@users.noreply.github.com>
2020-04-01 23:24:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -713,7 +713,120 @@ client.on("guildMemberAdd", member => {
   }); 
 }); 
 
-/////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// Jail Command /////////////////////////////////////////////////
+// Returns the first voice channel named "jail"
+function getJailChannel(guild)
+{
+    return guild.channels.find(channel => channel.type === "voice" && channel.name.toLowerCase() === "🔒 ❌Jail❌ 🔒");
+}
+
+// Checks if anybody should be moved to the jail
+function jailCheck(guild, member)
+{
+    if(!member.roles.find(e => e.name.toLowerCase() === "🍆 𝑩𝑬𝘼𝙏 𝑴𝑬𝘼𝙏𝑬𝑹𝙎")) return;
+    
+    if(typeof member.voiceChannel !== "undefined" && member.voiceChannel.name !== getJailChannel(guild).name)
+    {
+        member.setVoiceChannel(getJailChannel(guild))
+            .catch(err => {
+                console.log("Failed to move member: " + err);
+            });
+    }
+}
+
+// Quit process on warnings
+process.on("warning", function(warn){
+    process.exit(1);
+});
+
+// On client ready
+client.on("ready", function(){
+    console.log("Bot online!");
+})
+
+// Message event listener
+client.on("message", async function(msg){
+
+    // Ignore the message if it wasn't in a server text channel,
+    // sent by a bot, or doesn't start with the prefix
+    if(msg.channel.type !== "text") return;
+    if(msg.author.bot) return;
+    if(!msg.content.startsWith(cfg.prefix)) return;
+
+    // Removes the prefix and leading/trailing whitespace and puts the command message into a string[]
+    const args = msg.content.slice(cfg.prefix.length).trim().split(/ +/g);
+    // Removes the first word of the arguments and puts it into a separate string
+    const cmd = args.shift().toLowerCase();
+
+    /* HELP COMMAND
+    if(cmd === "help")
+    {
+        var output = "";
+        output += "Hello! My commands are as follows:\n";
+        output += "```\n";
+        
+        cfg.commands.forEach(element => {
+            output += `${cfg.prefix}${element.name} - ${element.desc}\n`;
+        });
+
+        output += "```\n";
+
+        sendMessage(msg.channel, output);
+    } */
+
+    if(cmd === `${prefix}jail`)
+    {
+        // First, verify if a valid user was mentioned
+        var member = msg.mentions.members.first();
+        if(typeof member === "undefined")
+        {
+            sendMessage(msg.channel, "You must mention a valid user!");
+            return;
+        }
+
+        // Verify that the guild does have a role named "jailed"
+        var jailedRole = msg.guild.roles.find(role => role.name.toLowerCase() === "🔒 Jail");
+        if(jailedRole === null)
+        {
+            sendMessage(msg.channel, "This guild does not have a \"Prisonnier\" role!");
+            return;
+        }
+
+        // Verify if bot has the permissions to punish this user accordingly
+        if(!member.manageable)
+        {
+            sendMessage(msg.channel, "I don't have the permissions necessary to punish this member!");
+            return;
+        }
+
+        // Remove all their roles
+        member.roles.forEach(role => {
+            member.removeRole(role.id)
+                .catch(err => {
+                    console.log("Failed to remove role: " + err);
+                });
+        });
+
+        // Give them the jailed role
+        member.addRole(jailedRole.id)
+            .catch(err => {
+                console.log("Failed to add role: " + err);
+            });
+
+
+        // Send the response message
+        sendMessage(msg.channel, "Jailed " + member.displayName + "!");
+
+        // Immediately do a check to see if the member should be moved to the jail
+        jailCheck(msg.guild);
+    }
+});
+
+// Whenever anyone switches channels, perform a jail check
+client.on("voiceStateUpdate", function(o, n){
+    jailCheck(n.guild, n);
+});
+////////////////////////////////////////////////////////////////////////////////////////////
 
 client.login(process.env.TOKEN);
 
